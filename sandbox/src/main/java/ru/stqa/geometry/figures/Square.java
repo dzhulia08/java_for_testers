a4f7bdfa00d8c9eca4d315fe4c6057464aadfb0d
dzhulia08 <dzhulia08@mail.ru>
2025-07-22 22:07:31 +0300

    Выполнен рефакторинг кода

--- sandbox/src/main/java/ru/stqa/geometry/figures/Square.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Square.java
@@ -2,16 +2,30 @@ package ru.stqa.geometry.figures;
 
 public class Square {
 
-    public static void printSquareArea(double side){
-        String text = String.format("Площадь квадрата со стороной %f = %f", side, area(side));
+    private double side; // описание структуры объекта перечисляет его свойства
+
+    public Square(double side) { // констуктор присваивает значения параметров в эти свойства
+        this.side = side;
+    }
+
+    public static void printSquareArea(Square s){
+        String text = String.format("Площадь квадрата со стороной %f = %f", s.side, s.area());
         System.out.println(text);
     }
 
-    public static double area(double a) {
-        return a * a;
+//    public static double area(double a) {
+//        return a * a;
+//    }
+//
+//    public static double perimeter(double a) {
+//        return 4 * a;
+//    }
+
+    public Object area() {
+        return this.side * this.side;
     }
 
-    public static double perimeter(double a) {
-        return 4 * a;
+    public double perimeter() {
+        return this.side * 4;
     }
 }
