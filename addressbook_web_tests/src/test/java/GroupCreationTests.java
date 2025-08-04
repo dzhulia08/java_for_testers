6942122000d24038e1bce1fb4e496a2f8aca8ce6
dzhulia08 <dzhulia08@mail.ru>
2025-08-04 20:34:34 +0300

    Лекция 3.2. Модельные классы для сущностей предметной области

--- addressbook_web_tests/src/test/java/GroupCreationTests.java
+++ addressbook_web_tests/src/test/java/GroupCreationTests.java
@@ -1,3 +1,4 @@
+import model.GroupData;
 import org.junit.jupiter.api.Test;
 
 public class GroupCreationTests extends TestBase {
@@ -5,13 +6,33 @@ public class GroupCreationTests extends TestBase {
     @Test
     public void canCrateGroup() {
         openGroupsPage();
-        createGroup("group name 1", "group header", "group footer");
+        createGroup(new GroupData("group name 1", "group header", "group footer"));
     }
 
     @Test
     public void canCrateGroupWithEmptyName() {
         openGroupsPage();
-        createGroup("", "", "");
+        createGroup(new GroupData());
+    }
+
+    @Test
+    public void canCrateGroupWithNameOnly() {
+        openGroupsPage();
+        var emptyGroup = new GroupData();
+        var groupWithName = emptyGroup.withName("some name");
+        createGroup(groupWithName);
+    }
+
+    @Test
+    public void canCrateGroupWithHeaderOnly() {
+        openGroupsPage();
+        createGroup(new GroupData().withHeader("some header"));
+    }
+
+    @Test
+    public void canCrateGroupWithFooterOnly() {
+        openGroupsPage();
+        createGroup(new GroupData().withFooter("some footer"));
     }
 
 }
