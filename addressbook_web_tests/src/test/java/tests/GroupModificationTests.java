5154f4b24df38289eabae96b99469e1a815cac75
dzhulia08 <dzhulia08@mail.ru>
2025-08-05 21:40:27 +0300

    Лекция 3.6. Повторение пройденного материала

---
+++ addressbook_web_tests/src/test/java/tests/GroupModificationTests.java
@@ -0,0 +1,15 @@
+package tests;
+
+import model.GroupData;
+import org.junit.jupiter.api.Test;
+
+public class GroupModificationTests extends TestBase {
+
+    @Test
+    public void canModifyGroup() {
+        if (!app.groups().isGroupPresent()){
+            app.groups().createGroup(new GroupData("group name 1", "group header", "group footer"));
+        }
+        app.groups().modifyGroup(new GroupData().withName("modified name"));
+    }
+}
