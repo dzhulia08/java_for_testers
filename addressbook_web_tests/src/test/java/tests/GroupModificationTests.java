d1da8b5d1e0cca37e10ea2adde6988e41471b201
dzhulia08 <dzhulia08@mail.ru>
2025-08-12 11:06:45 +0300

    Лекция 4.2. Количество элементов в коллекции

--- addressbook_web_tests/src/test/java/tests/GroupModificationTests.java
+++ addressbook_web_tests/src/test/java/tests/GroupModificationTests.java
@@ -7,7 +7,7 @@ public class GroupModificationTests extends TestBase {
 
     @Test
     public void canModifyGroup() {
-        if (!app.groups().isGroupPresent()){
+        if (app.groups().getCount() == 0){
             app.groups().createGroup(new GroupData("group name 1", "group middleName", "group lastName"));
         }
         app.groups().modifyGroup(new GroupData().withName("modified name"));
