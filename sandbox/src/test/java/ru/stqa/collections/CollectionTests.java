6bde3e4a44ddaa8880faa27e368f7f431490f14f
dzhulia08 <dzhulia08@mail.ru>
2025-09-01 22:22:34 +0300

    Лекция 7.4. Множества

--- sandbox/src/test/java/ru/stqa/collections/CollectionTests.java
+++ sandbox/src/test/java/ru/stqa/collections/CollectionTests.java
@@ -5,7 +5,9 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class CollectionTests {
 
@@ -22,14 +24,26 @@ public class CollectionTests {
 
     @Test
     void listTests() {
-        var list = new ArrayList<>(List.of("a", "b", "c")); // Создали список для хранения строк (имеет переменную длину)
-        Assertions.assertEquals(3, list.size()); // размер списка
+        var list = new ArrayList<>(List.of("a", "b", "c", "a")); // Создали список для хранения строк (имеет переменную длину)
+        Assertions.assertEquals(4, list.size()); // размер списка
         Assertions.assertEquals("a", list.get(0)); // получить элемент
 
         list.set(0, "d"); // записать значение в элемент с указанным индексом
         Assertions.assertEquals("d", list.get(0));
+    }
 
+    @Test
+    void setTests() {
+//        var set = Set.of("a", "b", "c"); // создали множество (в множестве элементы не могут повторяться)
+//        var set = Set.copyOf(List.of("a", "b", "c", "a"));
+        var set = new HashSet<>(List.of("a", "b", "c", "a"));
+        Assertions.assertEquals(3, set.size()); // размер множества
+        //var element = set.stream().findAny().get();
+        set.add("a");
+        Assertions.assertEquals(3, set.size());
 
+        set.add("d");
+        Assertions.assertEquals(4, set.size());
     }
 
 }
