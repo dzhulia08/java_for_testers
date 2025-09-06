11ccdb94b3a657456c70b246c0cbe8d10b76fc12
dzhulia08 <dzhulia08@mail.ru>
2025-09-06 13:23:25 +0300

    Лекция 8.2. Подготовка проекта для тестирования MantisBT - Задание №17: Подготовить новый проект для нового учебного приложения

---
+++ mantis_tests/src/test/java/ru/stqa/mantis/manager/ApplicationManager.java
@@ -0,0 +1,44 @@
+package ru.stqa.mantis.manager;
+
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.Properties;
+
+public class ApplicationManager {
+    private WebDriver driver;
+    private String browser;
+    private Properties properties;
+    private SessionHelper sessionHelper;
+
+    public void init(String browser, Properties properties) {
+        this.browser = browser;
+        this.properties = properties;
+    }
+
+    public WebDriver driver() {
+        if (driver == null) {
+            if ("firefox".equals(browser)) {
+                driver = new FirefoxDriver();
+            } else if ("chrome".equals(browser)) {
+                driver = new ChromeDriver();
+            } else {
+                throw new IllegalArgumentException(String.format("Unknown browser %s", browser));
+            }
+            driver.get(properties.getProperty("web.baseUrl"));
+            Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
+            driver.manage().window().setSize(new Dimension(1303, 833));
+        }
+        return driver;
+    }
+
+    public SessionHelper session() {
+        if (sessionHelper == null) {
+            sessionHelper = new SessionHelper(this);
+        }
+        return sessionHelper;
+    }
+
+}
