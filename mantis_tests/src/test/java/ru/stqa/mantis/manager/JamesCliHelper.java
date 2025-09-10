96cd7315bf72641f554d69ff85384ccb74000fb0
dzhulia08 <dzhulia08@mail.ru>
2025-09-10 12:49:28 +0300

    Лекция 8.4. Почтовый сервер James и управление через интерфейс командной строки (исправленный вариант)

--- mantis_tests/src/test/java/ru/stqa/mantis/manager/JamesCliHelper.java
+++ mantis_tests/src/test/java/ru/stqa/mantis/manager/JamesCliHelper.java
@@ -1,8 +1,10 @@
 package ru.stqa.mantis.manager;
 
-import net.bytebuddy.agent.builder.AgentBuilder;
 import org.openqa.selenium.io.CircularOutputStream;
 import org.openqa.selenium.os.CommandLine;
+import org.openqa.selenium.os.ExternalProcess;
+
+import java.time.Duration;
 
 public class JamesCliHelper extends HelperBase {
 
@@ -10,10 +12,18 @@ public class JamesCliHelper extends HelperBase {
         super(manager);
     }
 
-    public void addUser(String email, String password) {
+    public void addUser(String email, String password){
+//        ExternalProcess.builder()
+//                .command("java", "-cp", "\"james-server-jpa-app.lib/*\"", "org.apache.james.cli.ServerCmd", "AddUser", email, password)
+//                .directory(manager.property("james.workingDir"))
+//                .copyOutputTo(System.err)
+//                .start()
+//                .waitFor(Duration.ofHours(1));
+
         CommandLine cmd = new CommandLine("java", "-cp", "\"james-server-jpa-app.lib/*\"",
                 "org.apache.james.cli.ServerCmd",
                 "AddUser", email, password);
+        cmd.setWorkingDirectory(manager.property("james.workingDir"));
         CircularOutputStream out = new CircularOutputStream();
         cmd.copyOutputTo(out);
         cmd.execute();
